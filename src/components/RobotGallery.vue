13c49211e36cf4b274fa1ba541381dab9f067df9
Pcjmy <1716045191@qq.com>
2022-12-11 17:14:39 +0800

    完成首页

---
+++ src/components/RobotGallery.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="robotList">
+    <div class="cardContainer" v-for="robot in robots" :key="robot.id">
+      <img :src="robot.url" alt="robot">
+      <h2>{{robot.name}}</h2>
+      <p>{{robot.email}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RobotGallery',
+  props: {
+    robots: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cardContainer{
+  display: flex;
+  flex-direction: column;
+  background-color: #00BDAB;
+  border: 1px solid grey;
+  border-radius: 5px;
+  padding: 25px;
+  cursor: pointer;
+  backface-visibility: hidden;
+  transform: translateZ(0);
+  transition: transform 0.25s ease-out;
+}
+
+.cardContainer:hover{
+  transform:scale(1.05);
+}
+
+.robotList{
+  width: 85vw;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 20px;
+  margin: 0 auto;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
   <ContentBase>
-    首页
+    <RobotGallery :robots="robots"/>
   </ContentBase>
 </template>
 
 <script>
-import ContentBase from '../components/ContentBase';
+import ContentBase from '../components/ContentBase'
+import RobotGallery from '../components/RobotGallery'
+import robots from '../utils/robots.json'
 
 export default {
-  name: "HomeView",
+  name: 'HomeView',
   components: {
     ContentBase,
+    RobotGallery
+  },
+  setup() {
+    for(let robot of robots) {
+      robot.url = 'https://robohash.org/'+robot.id;
+    }
+    return {
+      robots
+    }
   }
-};
+}
 </script>
 
 <style scoped>
